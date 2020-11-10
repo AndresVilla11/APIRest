bff69963bebca0a4451d0f897fa167a293a49c5a
Andres Villamizar <a.villa11@hotmail.com>
2020-11-09 20:36:43 -0500

    first commit

---
+++ src/main/java/com/api/rest/html/ControllerHTML.java
@@ -0,0 +1,31 @@
+package com.api.rest.html;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/Home")
+public class ControllerHTML {
+	
+	@RequestMapping(value = { "Home", "" }, method = RequestMethod.GET)
+	public String controlDeInicio() {
+		return "Home";
+	}
+
+	@RequestMapping(value = "Crear_Persona", method = RequestMethod.POST)
+	public String controlDeFormulario() {
+		return "Formulario_datos";
+	}
+
+	@RequestMapping(value = "Listar_Personas", method = RequestMethod.GET)
+	public String controlDeListado() {
+		return "Listar-Personas";
+	}
+
+	@RequestMapping(value = { "Crear_Persona/Guardar" }, method = RequestMethod.GET)
+	public String controlDeRetorno() {
+		return "redirect:/";
+	}
+}
---
+++ src/main/java/com/api/rest/models/Model.java
@@ -0,0 +1,24 @@
+package com.api.rest.models;
+
+public class Model {
+
+	private String nombre;
+	private String apellido;
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return this.apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+}
